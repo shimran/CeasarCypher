bdba435bead81a6bbd4bc667c2734a7d5cd1e4eb
Shimran George <shimran.george@jivesoftware.com>
2016-05-27 20:56:53 -0700

    Add shift functionality and attempt to build out and message conversion

--- ceasercypher.py
+++ ceasercypher.py
@@ -6,12 +6,36 @@ def getMode:
     print("Do you want to encrypt (e) of decrypt (d) a message")
     entry=input().lower()
     if entry == 'e'
-        encryptMessage()
+        messagetoEncrypt()
+    if entry == 'd'
+        messageToDecrypt()
     else
-        decryptMessage()
+        print("Please enter a valid option")
 
-#
-def encryptMessage:
+ def shift():
+     shift = 0
+     while True:
+     print('Enter the shift number (1-%s)' % (26))
+     shift = int(input())
+     if (shift >= 1 and shift <= 26):
+         return shift
+
+def messagetoEncrypt(shiftNum, shift):
+    print("Please enter message to encrypt")
+    encryptMessage = input()
+    encryptMessageletters = list(encryptMessage)
+    for i in encryptMessage
+        encryptMessageAscii = x + shift
 
 
-def decryptMessage:
+def messageToDecrypt:
+    print ("Please enter message to decrypt")
+
+
+
+
+
+def encryptMessagef:
+
+
+def decryptMessagef:
