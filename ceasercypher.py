e0a84da78e7966afbc96ebb280af086643be4f7c
Shimran George <shimran.george@jivesoftware.com>
2016-05-27 18:58:21 -0700

    Setup basic methods

---
+++ ceasercypher.py
@@ -0,0 +1,17 @@
+#PYTHON CEASAR CYPHER FUNCTION
+
+#ENCRYPT OR DECRYPT?
+
+def getMode:
+    print("Do you want to encrypt (e) of decrypt (d) a message")
+    entry=input().lower()
+    if entry == 'e'
+        encryptMessage()
+    else
+        decryptMessage()
+
+#
+def encryptMessage:
+
+
+def decryptMessage:
